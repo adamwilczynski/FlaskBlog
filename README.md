16ad9b27455cc9695cea6a4cdda0a6edb2e92057
Adam <adam.wilcz7@gmail.com>
2022-06-14 11:38:21 +0200

    Created base template; Created routing

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # FlaskBlog
 
 This is an open-source repository for my blog website built after reading [Flask Web Development](https://www.oreilly.com/library/view/flask-web-development/9781491991725/) book by Miguel Grinberg.
+
+## Used technologies:
+
+### Front-end:
+
+- Bootstrap Studio with [Clean Blog](https://startbootstrap.com/theme/clean-blog) theme
+
+### Back-end:
+
+- Flask (Jinja2, )
--- blog.py
+++ blog.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+
+# from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
+
+# bootstrap = Bootstrap(app)
+
+BLOG_NAME = "EntropistA"
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", blog_name=BLOG_NAME)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", blog_name=BLOG_NAME)
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", blog_name=BLOG_NAME)
+
+
+@app.route("/post")
+def post():
+    return render_template("post.html", blog_name=BLOG_NAME)
